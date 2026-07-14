56bfd0f4ef7cd6e2395f93743e1bca058d1c7715
shreyaslande <shreyaslande200@gmail.com>
2026-07-14 22:49:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const userRouter = require("./routes/user");
+const Blog = require("./models/blog");
+const blogRouter = require("./routes/blog");
+const { checkForAuthenticationCookie } = require("./middleware/authentication");
+const app = express();
+const PORT = process.env.PORT || 8002;
+
+mongoose.connect(process.env.MONGO_URL).then((e)=>{console.log("mongodb connected")});
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
+
+app.use(express.json()); // For JSON requests
+app.use(express.urlencoded({ extended: false })); // For HTML form data
+app.use(cookieParser())
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
+app.use("/user", userRouter);
+app.use("/blog", blogRouter);
+
+
+app.get("/", async (req, res)=>{
+    const allBlogs = await (await Blog.find({}));
+    res.render("home",{
+        user: req.user,
+        blogs : allBlogs,
+    })
+})
+
+app.listen(PORT, ()=> console.log(`server started on PORT:${PORT}`))
---
+++ models/user.js
@@ -0,0 +1,76 @@
+const { Schema,  model } = require("mongoose");
+const { createHmac, randomBytes} = require("crypto");
+const { createTokenForUser} = require("../services/authentication")
+const userSchema = new Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageUrl: {
+        type: String,
+        default: "/images/default.png",
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    }
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function(){
+  const user = this;
+
+  if(!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString("hex");
+
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+});
+
+userSchema.statics.matchPasswordAndGenerateToken = async function(email, password) {
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw new Error("User not found!");
+  }
+
+  const salt = user.salt;
+  const hashedPassword = user.password;
+
+  const userProvidedHash = createHmac("sha256", user.salt)
+    .update(password)
+    .digest("hex");
+
+
+  if (user.password !== userProvidedHash) {
+    throw new Error("Incorrect password!");
+  }
+  
+  
+  const token = createTokenForUser(user);
+  return token
+};
+const User = model('user', userSchema);
+
+module.exports = User;
